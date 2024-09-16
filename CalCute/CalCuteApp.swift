0be5cd7dbafea7babfdf7c2c25df7ebe5b9c4535
Otávio Augusto <58156476+otavioaugusto-exe@users.noreply.github.com>
2024-09-15 21:23:15 -0300

    Initial Commit

---
+++ CalCute/CalCuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalCuteApp.swift
+//  CalCute
+//
+//  Created by Otávio Augusto on 15/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CalCuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
